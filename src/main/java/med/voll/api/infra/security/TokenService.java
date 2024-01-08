d2caaf248879f2ebdc9212f27991676b31c8fe29
Jhully Aires <jhullyolires@gmail.com>
2024-01-08 13:15:58 -0300

    Add security

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package med.voll.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,8 @@ import java.time.ZoneOffset;
 @Service
 public class TokenService {
 
+    private static final String ISSUER = "API Voll.med";
+
     @Value("${api.security.token.secret}")
     private String secret;
 
@@ -21,7 +24,7 @@ public class TokenService {
         try {
             var algoritmo = Algorithm.HMAC256(secret);
             return JWT.create()
-                    .withIssuer("API Voll.med")
+                    .withIssuer(ISSUER)
                     .withSubject(usuario.getLogin())
                     .withExpiresAt(dataExpiracao())
                     .sign(algoritmo);
@@ -30,7 +33,21 @@ public class TokenService {
         }
     }
 
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer(ISSUER)
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            exception.printStackTrace();
+            throw new RuntimeException("Token JWT expirado ou inválido");
+        }
+    }
+
     private Instant dataExpiracao() {
-        return LocalDateTime.now().plusHours(4).toInstant(ZoneOffset.of("-03:00"));
+        return LocalDateTime.now().plusYears(2).toInstant(ZoneOffset.UTC);
     }
 }
